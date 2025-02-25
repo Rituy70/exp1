32364025568bbd15790c26d212e616f37b39721d
Rituy70 <rituy6442@gmail.com>
2025-02-25 11:57:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add a simple JS to handle form interactions, alert messages, etc.
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Form Submitted Successfully!');
+  });
+  
